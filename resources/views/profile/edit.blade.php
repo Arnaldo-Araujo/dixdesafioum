9d48cd27f5946c070303cc9a3f71c2220d461998
Arnaldo Araujo <arnaldoaraujo@unitins.br>
2025-04-15 21:46:11 -0300

    bugfix: corrige validação da troca de senha e exibição de erros

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -64,6 +64,15 @@
                 </div>
 
                 <div class="card-body">
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul class="mb-0">
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endif
                     @if (session('success'))
                         <div class="alert alert-success">{{ session('success') }}</div>
                     @endif
